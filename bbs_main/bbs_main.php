a924246475c03362a113dfd5b3aedc0f2b150ec6
sk-tm46 <suzuki.tomoyuki46@gmail.com>
2021-06-03 20:48:37 +0900

    投稿内容1行表示

--- bbs_main/bbs_main.php
+++ bbs_main/bbs_main.php
@@ -23,15 +23,77 @@ else
 <title> ふれあい掲示板 </title>
 </head>
 <body>
+
+<?php
+//投稿内容を取得する
+try{
+$dsn = 'mysql:dbname=bbs;host=localhost;charset=utf8';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = 'SELECT * FROM mst_bbs WHERE 1';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$dbh = null;
+
+while(true)
+{
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+	$count = 0;
+	
+	if($rec==false)
+	{
+		break;
+	}
+	$bbs_post_no[$count] = $rec['no'];
+	$bbs_post_comment[$count] = $rec['comment'];
+		if($rec['image']=='')
+		{
+			$bbs_post_image[$count]='';
+		}
+		else
+		{
+			$bbs_post_image[$count]='<img src="gazou/'.$rec['image'].'">';
+		} //else終わり
+	$count = $count + 1;
+}
+
+$bbs_post_count = count($bbs_post_no);
+
+}
+catch (Exception $e)
+{
+	print 'ただいま障害により大変ご迷惑をお掛けしております。';
+	exit();
+}
+
+?>
+
 TYS掲示板<br/>
 <br/>
 コメント<br/>
-
 <form method="post" enctype="multipart/form-data">
 <input type="text" name="comment" style"width:200px"><br/>
 <input type="file" name="photo" id="sFiles"  style"width:400px">
 <input type="submit" formaction="bbs_post.php" name="svpost" value="投稿"><div id="photoMess"></div><br/>
-<input type="submit" formaction="bbs_reload.php" name="reload" value="最新を読み込む">
+<input type="submit" formaction="bbs_main.php" name="reload" value="最新を読み込む"><br/>
+
+<table border="1">
+<?php for($i=0;$i<$bbs_post_count;$i++)
+{
+?>
+	<tr>
+	<?php print $bbs_post_no[$i]; ?>
+	<?php print $bbs_post_comment[$i] ?>
+	<td><?php print $bbs_post_image[$i] ?></td>
+	</tr>
+<?php
+}
+?>
+</table>
 
 <script>
 function checkPhotoInfo()
